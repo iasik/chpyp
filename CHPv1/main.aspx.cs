8ceee859b1e7509e3b98d61dac8d49263ff8945a
ibrahim Asik <asikibrahim93@gmail.com>
2015-02-19 14:11:11 +0200

    main üzerinde grafik için sorgu testi yapıldı

--- CHPv1/main.aspx.cs
+++ CHPv1/main.aspx.cs
@@ -4,19 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
+using System.Data;
 namespace CHPv1
 {
     public partial class main : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-            if (Session["kadi"] == null)
+            grafik();
+            /*if (Session["kadi"] == null)
             {
                 Response.Redirect("login.aspx");
-            }
+            }*/
                        
+        }
+
+        protected void grafik()
+        {
+            SqlConnection con = new SqlConnection("Data Source=.\\chp;Initial Catalog=chpyp;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select count(PartiID) from people where PartiID = 1",con);
+            con.Close();
+
         }
     }
 }
